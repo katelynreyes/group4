4a02934582c3a265f4f54a14275b512b247683e1
katelynreyes <katelynreyes@unomaha.edu>
2024-03-21 17:33:11 -0500

    inital project

---
+++ catalog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from catalog.models import Pet, PetInstance
+
+# Register your models here.
+admin.site.register(Pet)
+admin.site.register(PetInstance)
+
---
+++ catalog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
+import uuid  # Required for unique pet instances
+
+
+class Pet(models.Model):
+    """model representing a pet"""
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    history = models.TextField(max_length=500, help_text='Enter a brief description of the pet')
+    breed = models.CharField(max_length=100, null=True, blank=True)
+    size = models.CharField(max_length=100, null=True, blank=True)
+
+    def get_absolute_url(self):
+        """returns the url to access the pet """
+        return reverse('pet_detail', args=[str(self.id)])
+
+    def __str__(self):
+        """string for representing the Model object"""
+        return self.name
+
+
+class PetInstance(models.Model):
+    """model representing a specific pet that can have an appointment created to visit"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this pet")
+    pet = models.ForeignKey('Pet', on_delete=models.RESTRICT, null=True)
+
+    def __str__(self):
+        """string for representing the Model object"""
+        return f'{self.id} ({self.pet.name})'
