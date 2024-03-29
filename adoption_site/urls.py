17472e6d08e902ec7ec01d55bb4c0b40636ea415
katelynreyes <katelynreyes@unomaha.edu>
2024-03-28 19:20:47 -0500

    Home page
    created home page, nav bar, css

--- adoption_site/urls.py
+++ adoption_site/urls.py
@@ -7,4 +7,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,12 @@
+# from .models import Pet, PetInstance
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+    context={'test':'Testing'}
+    return render(request, 'index.html', context=context)
+
+from django.shortcuts import render
+
+
