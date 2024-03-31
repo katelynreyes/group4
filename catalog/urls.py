c915e20ea8bb51ed69701a9f0f4566e9cd49f22e
alesera2024 <aleser.akhtar@gmail.com>
2024-03-31 03:37:17 -0500

    Volunteer Information
    Made the volunteer Information page functional.

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('', views.home, name='home'),  # The main page
+    path('volunteer/', views.volunteer, name='volunteer'),  # Volunteer information page
 ]
-
-
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,26 @@
-# from .models import Pet, PetInstance
 from django.shortcuts import render
 
+
+# Existing import statements...
+# from .models import Pet, PetInstance
 
 def index(request):
     """View function for home page of site."""
-    context={'test':'Testing'}
-    return render(request, 'index.html', context=context)
-
-from django.shortcuts import render
+    context = {'test': 'Testing'}
+    return render(request, 'catalog/index.html', context=context)
 
 
+def home(request):
+    """View function for home page of site."""
+    # Context can contain any data you want to pass to the template
+    return render(request, 'catalog/base.html', {})  # Changed 'home.html' to 'base.html'
+
+
+def volunteer(request):  # Renamed from volunteer_info to volunteer
+    """View function for the volunteer page."""
+    return render(request, 'catalog/volunteer.html', {})  # The context is empty for now
+
+
+def volunteer(request):
+    # Render the volunteer_info.html template
+    return render(request, 'catalog/volunteer_info.html')
