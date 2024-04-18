e0456a7e0d4c0d37d749c219e3a13db87fb7d867
echavez-uno <enriquechavez@unomaha.edu>
2024-04-18 17:45:02 -0500

    User account changes

--- catalog/views.py
+++ catalog/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Pet
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Pet, PetInstance
+from django.shortcuts import render, redirect
+from .forms import AdoptionForm
+from catalog.forms import AdoptionForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-
-# Existing import statements...
 
 
 def index(request):
@@ -30,3 +33,24 @@ class PetDetailView(LoginRequiredMixin, generic.DetailView):
 
 class PetListView(LoginRequiredMixin, generic.ListView):
     model = Pet
+
+
+def adoption_application(request):
+    if request.method == 'POST':
+        # If the form is submitted via POST, instantiate the AdoptionForm with the submitted data
+        form = AdoptionForm(request.POST)
+        if form.is_valid():
+            # If the form is valid, save the form data to the database
+            form.save()
+            # Redirect to a success page
+            return redirect('application_submitted')
+    else:
+        # If the request method is not POST (e.g., GET), instantiate an empty AdoptionForm
+        form = AdoptionForm()
+
+    # Pass the form instance to the template context
+    return render(request, 'adoption_form.html', {'form': form})
+
+# defines the view for the success page
+def application_submitted(request):
+    return render(request, 'application_submitted.html')
