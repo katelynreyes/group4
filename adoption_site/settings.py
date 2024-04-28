5528b1f3ceee85dd8378cf535f898b5452518f33
echavez-uno <enriquechavez@unomaha.edu>
2024-04-27 22:20:40 -0500

    changes

--- adoption_site/settings.py
+++ adoption_site/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
     'register.apps.RegisterConfig',
+    'django.contrib.admin',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
